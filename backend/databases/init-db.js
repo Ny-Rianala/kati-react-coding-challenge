a3059eb3f0b25317217e29e9ee028fc802323ef5
Rinon <rinon.ten@onja.org>
2024-10-01 12:38:09 +0300

    Update db initialisation function

--- backend/databases/init-db.js
+++ backend/databases/init-db.js
@@ -1,24 +1,17 @@
-const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
-
- const productsDb = new sqlite3.Database(path.resolve(__dirname, './products.db'), (err) => {
-  if (err) {
-    console.error("Error connecting to the database", err);
-  } else {
-    console.log("Connected to SQLite database");
-  }
-});
-
 const sqlite = require('sqlite3').verbose();
 
-// Connect to SQLite database
-const usersDb = new sqlite.Database('./users.db', (err) => {
-  if (err) {
-    console.error("Error connecting to the database", err);
-  } else {
-    console.log("Connected to SQLite database");
-  }
-});
+const connectToDatabase = (dbName) => {
+  return new sqlite.Database(path.join(__dirname, dbName), (err) => {
+    if (err) {
+      console.error(`Error connecting to the ${dbName} database`, err);
+    } else {
+      console.log(`Connected to SQLite ${dbName} database`);
+    }
+  });
+};
 
+const usersDb = connectToDatabase('users.db');
+const productsDb = connectToDatabase('products.db');
 
 module.exports = { productsDb, usersDb };
