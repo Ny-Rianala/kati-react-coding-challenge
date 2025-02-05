88196c4da266a31796930c86f34acddc6a676ea1
Kati Fanomezantsoa <kati@northstreetcreative.com>
2025-02-05 20:40:53 +0300

    added login component

--- frontend/sell-smart/src/App.js
+++ frontend/sell-smart/src/App.js
@@ -1,12 +1,32 @@
-import './App.css';
-import Header from "./components/Header"
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Login from './components/Login';  // Import Login component
 
-function App() {
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [token, setToken] = useState('');
+  const [showLogin, setShowLogin] = useState(false);
+
+  const handleLoginSuccess = (token) => {
+    setToken(token);
+    setIsAuthenticated(true);
+    setShowLogin(false);  // Hide the login modal on success
+  };
+
   return (
-    <div className="App">
-        <Header />
+    <div>
+      <Header 
+        isAuthenticated={isAuthenticated}
+        onLogin={() => setShowLogin(true)}  // Show login modal
+        onLogout={() => {
+          setIsAuthenticated(false);
+          setToken('');
+        }}
+      />
+      {showLogin && <Login onLoginSuccess={handleLoginSuccess} />}
     </div>
   );
-}
+};
 
 export default App;
+
